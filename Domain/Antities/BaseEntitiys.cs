ad4b20c65fb2ddf221ab93c69e36b89612e61e63
Timur <timxxxasanov@mail.ru>
2024-03-06 20:24:03 +0200

    Add project files.

---
+++ Domain/Antities/BaseEntitiys.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Antities
+{
+    /// <summary>
+    /// Базовый класс для всех сущностей
+    /// </summary>
+    public abstract class BaseEntitiys
+    {
+        /// <summary>
+        /// Уникальный идентификатор сущности
+        /// </summary>
+        protected Guid Id { get; set; }
+        /// <summary>
+        /// Дата создания сущности
+        /// </summary>
+        protected DateTime CreatedDate { get; set; }
+        /// <summary>
+        /// Метод для сравнения сущностей по айди
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public override bool Equals(object? obj)
+        {
+            if (obj == null) 
+                return false;
+            if(obj is not BaseEntitiys entitiys)
+                return false;
+            if(Id!=entitiys.Id)
+                return false;
+            return true;
+            
+        }
+        /// <summary>
+        /// todo: Домашнее задание понять почему икуалс заставил определить гетхэш код и переписать с исп того что узнаю
+        /// </summary>
+        /// <returns></returns>
+        public override int GetHashCode()
+        { 
+            return new NotImplementedException;
+        }
+    }
+}
---
+++ Domain/Antities/Person.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Antities
+{
+    /// <summary>
+    /// Основная сущность персоны(человека)
+    /// </summary>
+    public class Person : BaseEntitiys
+    {
+        string name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+        string surname;
+        int age { get; set; }
+        DateTime birthday { get; }
+        /// <summary>
+        /// Макс полно описать сущность персона(нужна для того чтобы собирать инфу о человеке(собрать параметры о человеке др и тд))
+        /// Сделать добавление через конструктор
+        /// </summary>
+        public Person(string name, string surname, int age)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.age = age;
+        }
+        public Person(string name, string surname, DateTime birthday)
+        {
+            this.name = name;
+            this.surname = surname;
+            this.birthday = birthday;
+            DateTime now = DateTime.Today;
+            this.age = now.Year - birthday.Year - 1 + ((now.Month > birthday.Month || now.Month == birthday.Month && now.Day >= birthday.Day) ? 1 : 0);
+        }
+    }
+}
+
